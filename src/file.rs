5e38be73c5617beea8e388eabdae7d199a067b4a
John Kugelman <john@kugelman.name>
2021-09-09 22:42:45 -0400

    Lookup lump ranges
    Lump names aren't unique. Detect and handle non-uniqueness robustly with
    a LumpIndex enum.

--- src/file.rs
+++ src/file.rs
@@ -1,13 +1,27 @@
 use byteorder::{LittleEndian, ReadBytesExt};
-use std::{cell::RefCell, collections::HashMap, convert::TryInto, fs::File, io::{self, BufReader, Read, Seek, SeekFrom}, path::Path, rc::Rc};
+use indexmap::IndexMap;
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    convert::TryInto,
+    fs::File,
+    io::{self, BufReader, Read, Seek, SeekFrom},
+    path::Path,
+    rc::Rc,
+};
 
 pub struct WadFile {
-    wad_type: WadType,
-    lump_cache: RefCell<HashMap<String, Rc<[u8]>>>,
-
     file: RefCell<BufReader<File>>,
-    lumps_by_index: Vec<Lump>,
-    lumps_by_name: HashMap<String, usize>,
+    wad_type: WadType,
+    lump_locations: Vec<Lump>,
+    lump_indices: HashMap<String, LumpIndex>,
+    lump_cache: RefCell<Vec<Option<Rc<[u8]>>>>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum LumpIndex {
+    Unique(usize),
+    NotUnique,
 }
 
 impl WadFile {
@@ -19,47 +33,76 @@ impl WadFile {
 
         let directory =
             Directory::read_from(&mut file, header.directory_offset, header.lump_count)?;
-        let lump_vec = directory.lumps;
-        let mut lump_map = HashMap::new();
-        for (i, lump) in lump_vec.iter().enumerate() {
-            lump_map.insert(lump.name.clone(), i);
+        let lump_locations = directory.lumps;
+
+        let mut lump_indices = HashMap::new();
+        for (index, lump) in lump_locations.iter().enumerate() {
+            lump_indices
+                .entry(lump.name.clone())
+                .and_modify(|e| *e = LumpIndex::NotUnique)
+                .or_insert(LumpIndex::Unique(index));
         }
+
+        let lump_cache = vec![None; lump_locations.len()];
 
         Ok(WadFile {
             wad_type: header.wad_type,
-            lump_cache: RefCell::new(HashMap::new()),
-
             file: RefCell::new(file),
-            lumps_by_index: lump_vec,
-            lumps_by_name: lump_map,
+            lump_locations,
+            lump_indices,
+            lump_cache: RefCell::new(lump_cache),
         })
     }
 
     pub fn lump(&self, name: &str) -> io::Result<Rc<[u8]>> {
-        let mut lump_cache = self.lump_cache.borrow_mut();
+        let index = self.lump_index(name)?;
 
-        // Already in cache?
-        if let Some(contents) = lump_cache.get(name) {
-            return Ok(contents.clone());
+        let mut lump_cache = self.lump_cache.borrow_mut();
+        let cached_contents = &mut lump_cache[index];
+
+        if cached_contents.is_none() {
+            // Get lump metadata.
+            let index = self.lump_index(name)?;
+            let lump = &self.lump_locations[index];
+
+            // Read lump from file.
+            let mut file = self.file.borrow_mut();
+            let mut contents = vec![0u8; lump.size.try_into().unwrap()];
+            file.seek(SeekFrom::Start(lump.offset.into()))?;
+            file.read_exact(&mut contents)?;
+
+            // Add to cache.
+            let contents: Rc<[u8]> = contents.into_boxed_slice().into();
+            *cached_contents = Some(contents.clone());
         }
 
-        // Get lump metadata.
-        let index = *self.lumps_by_name.get(name).ok_or_else(|| {
+        Ok(cached_contents.as_ref().unwrap().clone())
+    }
+
+    pub fn lumps_between(&self, start: &str, end: &str) -> io::Result<IndexMap<String, Rc<[u8]>>> {
+        let start_index = self.lump_index(start)?;
+        let end_index = self.lump_index(end)?;
+
+        let mut lumps = IndexMap::with_capacity(end_index - start_index - 2);
+        for index in (start_index + 1)..(end_index - 1) {
+            let name = &self.lump_locations[index].name;
+            lumps.insert(name.clone(), self.lump(name)?);
+        }
+        Ok(lumps)
+    }
+
+    fn lump_index(&self, name: &str) -> io::Result<usize> {
+        let index = self.lump_indices.get(name).copied().ok_or_else(|| {
             io::Error::new(io::ErrorKind::NotFound, format!("no lump named {}", name))
         })?;
-        let lump = self.lumps_by_index.get(index).unwrap();
 
-        // Read lump from file.
-        let mut file = self.file.borrow_mut();
-        let mut contents = vec![0u8; lump.size.try_into().unwrap()];
-        file.seek(SeekFrom::Start(lump.offset.into()))?;
-        file.read_exact(&mut contents)?;
-
-        // Add to cache.
-        let contents: Rc<[u8]> = contents.into_boxed_slice().into();
-        lump_cache.insert(name.to_owned(), contents.clone());
-
-        Ok(contents)
+        match index {
+            LumpIndex::Unique(index) => Ok(index),
+            LumpIndex::NotUnique => Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!("{} not unique", name),
+            )),
+        }
     }
 }
 
@@ -122,7 +165,7 @@ impl Directory {
     ) -> io::Result<Self> {
         file.seek(SeekFrom::Start(directory_offset.into()))?;
 
-        let mut lumps = Vec::new();
+        let mut lumps = Vec::with_capacity(lump_count.try_into().unwrap());
 
         for _ in 0..lump_count {
             let offset = file.read_u32::<LittleEndian>()?;
@@ -134,7 +177,7 @@ impl Directory {
                 .trim_end_matches('\0')
                 .to_owned();
 
-            lumps.push(Lump { offset, size, name })
+            lumps.push(Lump { offset, size, name });
         }
 
         Ok(Directory { lumps })
@@ -156,19 +199,11 @@ mod test {
     fn header() -> io::Result<()> {
         let wad = test_wad("doom.wad")?;
         assert_eq!(wad.wad_type, WadType::Initial);
-        assert_eq!(wad.lumps_by_index.len(), 1264);
-        assert_eq!(wad.lumps_by_index[0].name, "PLAYPAL");
-        assert_eq!(wad.lumps_by_index[1].name, "COLORMAP");
-        assert_eq!(wad.lumps_by_index[2].name, "ENDOOM");
-        assert_eq!(wad.lumps_by_index[3].name, "DEMO1");
-    
+        assert_eq!(wad.lump_locations.len(), 1264);
+
         let wad = test_wad("killer.wad")?;
         assert_eq!(wad.wad_type, WadType::Patch);
-        assert_eq!(wad.lumps_by_index.len(), 55);
-        assert_eq!(wad.lumps_by_index[0].name, "E1M1");
-        assert_eq!(wad.lumps_by_index[1].name, "THINGS");
-        assert_eq!(wad.lumps_by_index[2].name, "LINEDEFS");
-        assert_eq!(wad.lumps_by_index[3].name, "SIDEDEFS");
+        assert_eq!(wad.lump_locations.len(), 55);
 
         Ok(())
     }
@@ -181,10 +216,33 @@ mod test {
         assert_eq!(wad.lump("E1M1")?.len(), 0);
 
         // Test cache.
-        assert!(!wad.lump_cache.borrow().contains_key("PNAMES"));
+        assert!(wad.lump_cache.borrow()[wad.lump_index("PNAMES")?].is_none());
         assert_eq!(wad.lump("PNAMES")?.len(), 2804);
-        assert!(wad.lump_cache.borrow().contains_key("PNAMES"));
+        assert!(wad.lump_cache.borrow()[wad.lump_index("PNAMES")?].is_some());
         assert_eq!(wad.lump("PNAMES")?.len(), 2804);
+
+        Ok(())
+    }
+
+    #[test]
+    fn detect_duplicates() -> io::Result<()> {
+        let wad = test_wad("doom.wad")?;
+
+        assert!(wad.lump_index("E1M1").is_ok());
+        assert!(wad.lump_index("THINGS").is_err());
+        assert!(wad.lump_index("VERTEXES").is_err());
+        assert!(wad.lump_index("SECTORS").is_err());
+        
+        Ok(())
+    }
+
+    #[test]
+    fn lumps_between() -> io::Result<()> {
+        let wad = test_wad("doom.wad")?;
+
+        let sprites = wad.lumps_between("S_START", "S_END")?;
+        assert_eq!(sprites.len(), 482);
+        assert_eq!(sprites.get_index(100).unwrap().0, "SARGB5");
 
         Ok(())
     }
